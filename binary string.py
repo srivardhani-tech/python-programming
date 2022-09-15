af300e71ea60346e90367c0d0a2251e08eccc47f
srivardhani-tech <110552310+srivardhani-tech@users.noreply.github.com>
2022-09-15 14:26:33 +0530

    Create binary string.py

---
+++ binary string.py
@@ -0,0 +1,40 @@
+def maxProfit(price, n):
+
+ profit = [0]*n
+
+ max_price = price[n-1]
+
+ for i in range(n-2, 0, -1):
+
+  if price[i] > max_price:
+   max_price = price[i]
+
+  profit[i] = max(profit[i+1], max_price - price[i])
+
+ min_price = price[0]
+
+ for i in range(1, n):
+
+  if price[i] < min_price:
+   min_price = price[i]
+
+  profit[i] = max(profit[i-1], profit[i]+(price[i]-min_price))
+
+ result = profit[n-1]
+
+ return result
+a=[]
+n=int(input("Enter number of items:"))
+for i in range(n):
+    a.append(int(input()))
+print ("Maximum profit is", maxProfit(a, len(a)))
+
+>>>#output:
+Enter number of items:6
+7
+1
+5
+3
+6
+4
+Maximum profit is 7
