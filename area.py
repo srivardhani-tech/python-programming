d5f6a5bb8971e124dec02c52413c5e748fe9a1f6
srivardhani-tech <110552310+srivardhani-tech@users.noreply.github.com>
2022-09-15 14:20:27 +0530

    Create area.py

---
+++ area.py
@@ -0,0 +1,20 @@
+print("area of triangle")
+a=int(input("enter the height of the triangle"))
+b=int(input("enter the base of the triangle"))
+area=0.5*a*b
+print("the area of triangle is",area)
+print("area of circle")
+r=int(input("enter the radius of circle"))
+ar=3.14*r*r
+print("the area of circle is",ar)
+
+#>>>output:
+#op1:
+enter the number of new louves ordered5
+enter the number of old louves ordered3
+the regular price for the new loaf is  185.00
+the total price for the new loaf is 925.00
+the total price for the old loaf is 333.00
+total is :
+1258.00
+  
