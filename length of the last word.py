18bf6593e4f7fd6bf8a36de561e41c2954679997
srivardhani-tech <110552310+srivardhani-tech@users.noreply.github.com>
2022-09-15 14:55:25 +0530

    Create length of the last word.py

---
+++ length of the last word.py
@@ -0,0 +1,19 @@
+def lengthOfLastWord(a):
+	l = 0
+	x = a.strip()
+
+	for i in range(len(x)):
+		if x[i] == " ":
+			l = 0
+		else:
+			l += 1
+	return l
+if __name__ == "__main__":
+	inp = input("enter the string")
+	print("The length of last word is",lengthOfLastWord(inp))
+  
+  >>output:
+    enter the stringthe wind is blowing fast
+The length of last word is 4
+
+
